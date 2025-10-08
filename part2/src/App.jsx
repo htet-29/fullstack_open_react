cf1fad425e4fe6b2ae1d30bb95c96501fd4d14c3
Htet Hlaing Win <htethlaingwin.bsn29@gmail.com>
2025-10-08 23:23:09 +0630

    Style

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,11 +1,14 @@
 import { useState, useEffect } from "react";
 import noteService from "./services/note";
 import Note from "./components/Note";
+import Notification from "./components/Notification";
+import Footer from "./components/Footer";
 
 const App = () => {
     const [notes, setNotes] = useState([]);
     const [newNote, setNewNote] = useState('a new note...');
     const [showAll, setShowAll] = useState(true)
+    const [errorMessage, setErrorMessage] = useState(null);
     
     const hook = () => {
         console.log("effect");
@@ -48,10 +51,14 @@ const App = () => {
                 setNotes(notes.map(note => note.id === id ? returnNote : note));
             })
             .catch((error) => {
-                alert(
-                    `the note '${note.content}' was already deleted from server`
+                setErrorMessage(
+                    `Note '${note.content}' was already removed from server`
                 );
-                setNotes(notes.filter(n => n.id !== id));
+                
+                setTimeout(() => {
+                    setErrorMessage(null)
+                }, 5000);
+                setNotes(notes.filter(n => n.id !== id))
             })
     }
 
@@ -61,6 +68,7 @@ const App = () => {
     return (
         <div>
             <h1>Notes</h1>
+            <Notification message={errorMessage}/>
             <div>
                 <button onClick={() => setShowAll(!showAll)}>
                     show {showAll ? 'important' : 'all'}
@@ -75,6 +83,7 @@ const App = () => {
                 <input onChange={handleNoteChange} value={newNote} />
                 <button type="submit">save</button>
             </form>
+            <Footer />
         </div>
     )
 }
