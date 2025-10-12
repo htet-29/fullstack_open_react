1ba63ae58acf08a6362d903577d2d81ff174c4b3
Htet Hlaing Win <htethlaingwin.bsn29@gmail.com>
2025-10-12 13:18:25 +0630

    deleting and updating

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -45,11 +45,10 @@ app.get('/api/notes/:id', (request, response, next) => {
   .catch(error => next(error));
 });
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = request.params.id;
-  notes = notes.filter(note => note.id !== id);
-  
-  response.status(204).end();
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id)
+    .then(_result => response.status(204).end())
+    .catch(error => next(error));
 });
 
 app.post('/api/notes', (request, response) => {
@@ -68,6 +67,25 @@ app.post('/api/notes', (request, response) => {
     response.json(savedNote);
   })
 });
+
+app.put('/api/notes/:id', (request, response, next) => {
+  const { content, important } = request.body;
+  
+  Note.findById(request.params.id)
+    .then(note => {
+      if (!note) {
+        return response.status(404).send('<h1>Not Found!</h1>').end();
+      }
+      
+      note.content = content;
+      note.important = important;
+      
+      return note.save().then(updatedNote => {
+        response.json(updatedNote);
+      });
+    })
+    .catch(error => next(error));
+})
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
