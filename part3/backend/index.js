76d30e348865f5fea5617342a9cbfa36c9c739dd
Htet Hlaing Win <htethlaingwin.bsn29@gmail.com>
2025-10-12 13:06:43 +0630

    adding error handling middleware

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -24,7 +24,7 @@ app.use(morgan(customFormat));
 
 
 // All API Endpoints
-app.get('/', (request, response) => {
+app.get('/info', (request, response) => {
   response.send('<h1>Hello World!</h1>');
 });
 
@@ -34,10 +34,15 @@ app.get('/api/notes', (request, response) => {
   })
 });
 
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response, next) => {
   Note.findById(request.params.id).then(note => {
-    response.json(note);
+    if (note) {
+      response.json(note);
+    } else {
+      response.status(404).end();
+    }
   })
+  .catch(error => next(error));
 });
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -63,6 +68,25 @@ app.post('/api/notes', (request, response) => {
     response.json(savedNote);
   })
 });
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+  
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' });
+  }
+  
+  next(error);
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
