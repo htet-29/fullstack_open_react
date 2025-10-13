cb1e7110be8001792ada69f8e44ac322c4ff505f
Htet Hlaing Win <htethlaingwin.bsn29@gmail.com>
2025-10-13 07:35:22 +0630

    adding mongoose build-in validation

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -51,7 +51,7 @@ app.delete('/api/notes/:id', (request, response, next) => {
     .catch(error => next(error));
 });
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
   const body = request.body;
   
   if (!body.content) {
@@ -66,6 +66,7 @@ app.post('/api/notes', (request, response) => {
   note.save().then(savedNote => {
     response.json(savedNote);
   })
+  .catch(error => next(error));
 });
 
 app.put('/api/notes/:id', (request, response, next) => {
@@ -99,6 +100,8 @@ const errorHandler = (error, request, response, next) => {
   
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' });
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).send({ error: error.message });
   }
   
   next(error);
